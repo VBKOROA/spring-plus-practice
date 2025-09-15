31742d8f144eae4077d20e75770bc5cff93079d2
abko <abko121115@gmail.com>
2025-09-15 11:22:50 +0900

    feat: 파일 업로드 관련 예외처리 구현

--- src/main/java/org/example/expert/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/expert/config/GlobalExceptionHandler.java
@@ -7,10 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartException;
+
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -30,6 +35,21 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, Object>> handleServerException(ServerException ex) {
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
         return getErrorResponse(status, ex.getMessage());
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public ResponseEntity<Map<String, Object>> handleMaxUploadSizeExceeded(MaxUploadSizeExceededException ex) {
+        HttpStatus status = HttpStatus.PAYLOAD_TOO_LARGE;
+        String message = "업로드한 파일의 크기가 허용된 최대 크기를 초과했습니다(최대 크기 5MB).";
+        return getErrorResponse(status, message);
+    }
+
+    @ExceptionHandler(MultipartException.class)
+    public ResponseEntity<Map<String, Object>> handleMultipartException(MultipartException ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        String message = "파일 업로드 처리 중 오류가 발생했습니다.";
+        log.warn("Uploading Error: {}", ex.getMessage());
+        return getErrorResponse(status, message);
     }
 
     public ResponseEntity<Map<String, Object>> getErrorResponse(HttpStatus status, String message) {

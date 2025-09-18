4dc5d9a12a107909155d0b3dfc79a7b22c55bb9a
abko <abko121115@gmail.com>
2025-09-18 10:41:39 +0900

    refactor: clean yellow lines

--- src/main/java/org/example/expert/config/auth/JwtUtil.java
+++ src/main/java/org/example/expert/config/auth/JwtUtil.java
@@ -6,11 +6,9 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.example.expert.domain.common.exception.ServerException;
 import org.example.expert.domain.user.enums.UserRole;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import java.security.Key;
 import java.util.Base64;

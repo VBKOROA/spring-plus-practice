57db0d1219b37b7cb6d38150e3874d04c68d2f08
abko <abko121115@gmail.com>
2025-09-23 11:58:46 +0900

    refactor: 비밀번호를 UserPasswordVO로 분리하여 코드 가독성 향상

--- src/main/java/org/example/expert/domain/user/service/UserService.java
+++ src/main/java/org/example/expert/domain/user/service/UserService.java
@@ -31,20 +31,12 @@ public class UserService {
 
     @Transactional
     public void changePassword(long userId, UserChangePasswordRequest userChangePasswordRequest) {
-        validateNewPassword(userChangePasswordRequest);
-
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new InvalidRequestException("User not found"));
+        String newPassword = userChangePasswordRequest.getNewPassword();
+        String oldPassword = userChangePasswordRequest.getOldPassword();
 
-        if (passwordEncoder.matches(userChangePasswordRequest.getNewPassword(), user.getPassword())) {
-            throw new InvalidRequestException("새 비밀번호는 기존 비밀번호와 같을 수 없습니다.");
-        }
-
-        if (!passwordEncoder.matches(userChangePasswordRequest.getOldPassword(), user.getPassword())) {
-            throw new InvalidRequestException("잘못된 비밀번호입니다.");
-        }
-
-        user.changePassword(passwordEncoder.encode(userChangePasswordRequest.getNewPassword()));
+        user.changePassword(newPassword, oldPassword, passwordEncoder);
     }
 
     @Transactional
@@ -61,13 +53,5 @@ public class UserService {
         user.changeProfile(profileUrl);
 
         return jwtUtil.createToken(userId, user.getEmail(), user.getNickname(), user.getProfile(), user.getUserRole());
-    }
-
-    private static void validateNewPassword(UserChangePasswordRequest userChangePasswordRequest) {
-        if (userChangePasswordRequest.getNewPassword().length() < 8 ||
-                !userChangePasswordRequest.getNewPassword().matches(".*\\d.*") ||
-                !userChangePasswordRequest.getNewPassword().matches(".*[A-Z].*")) {
-            throw new InvalidRequestException("새 비밀번호는 8자 이상이어야 하고, 숫자와 대문자를 포함해야 합니다.");
-        }
     }
 }

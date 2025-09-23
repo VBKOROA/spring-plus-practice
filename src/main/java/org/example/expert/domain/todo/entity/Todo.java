001ea55543af41ccf4abff7012d73107a1edc423
abko <abko121115@gmail.com>
2025-09-23 11:01:49 +0900

    refactor: Todo 엔티티에 담당자 추가 로직을 메서드로 분리하여 코드 가독성 향상

--- src/main/java/org/example/expert/domain/todo/entity/Todo.java
+++ src/main/java/org/example/expert/domain/todo/entity/Todo.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.common.entity.Timestamped;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.manager.entity.Manager;
 import org.example.expert.domain.user.entity.User;
+import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,5 +41,19 @@ public class Todo extends Timestamped {
         this.weather = weather;
         this.user = user;
         this.managers.add(new Manager(user, this));
+    }
+
+    public Manager addManager(User requester, User targetUser) {
+        if (this.getUser() == null || !ObjectUtils.nullSafeEquals(user.getId(), this.getUser().getId())) {
+            throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 유효하지 않거나, 일정을 만든 유저가 아닙니다.");
+        }
+
+        if (ObjectUtils.nullSafeEquals(requester.getId(), targetUser.getId())) {
+            throw new InvalidRequestException("일정 작성자는 본인을 담당자로 등록할 수 없습니다.");
+        }
+
+        Manager newManagerUser = new Manager(targetUser, this);
+        this.managers.add(newManagerUser);
+        return newManagerUser;
     }
 }
